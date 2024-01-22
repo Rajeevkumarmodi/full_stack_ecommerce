eb7e2fd57508ef23181f5ccabe4064e88f9c588b
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2024-01-22 16:58:12 +0530

    custome api response

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -1,5 +1,6 @@
 import User from "../models/user.models.js";
 import bcrypt from "bcrypt";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 export const registerController = async (req, res) => {
   const { name, email, password, profilePic } = req.body;
@@ -7,14 +8,19 @@ export const registerController = async (req, res) => {
   if (!name || !email || !password || !profilePic) {
     return res
       .status(404)
-      .json({ success: false, message: "all fields are required" });
+      .json(new ApiResponse(404, "all fields are required", false));
   }
 
   if (!email.includes("@") || !email.includes(".")) {
-    return res.status(404).json({
-      success: false,
-      message: "Email is not valid please enter valid emain",
-    });
+    return res
+      .status(404)
+      .json(
+        new ApiResponse(
+          404,
+          "Email is not valid please enter valid email",
+          false
+        )
+      );
   }
 
   try {
@@ -22,7 +28,7 @@ export const registerController = async (req, res) => {
     if (existingUser) {
       return res
         .status(404)
-        .json({ success: false, message: "User already exist" });
+        .json(new ApiResponse(404, "User already exist", false));
     } else {
       const salt = bcrypt.genSaltSync(13);
       const hashPassword = bcrypt.hashSync(password, salt);
@@ -35,14 +41,12 @@ export const registerController = async (req, res) => {
       });
       await newUser.save();
 
-      res.status(201).json({
-        success: true,
-        message: "Register successfully",
-        data: newUser,
-      });
+      res
+        .status(201)
+        .json(new ApiResponse(201, "Registration successfull", true));
     }
   } catch (error) {
     console.log(error);
-    res.status(404).json({ success: false, message: error.message });
+    res.status(500).json(new ApiResponse(500, error.message, false));
   }
 };
